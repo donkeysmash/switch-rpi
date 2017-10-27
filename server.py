2185e090cba2b40055beff2fe312945e88bb6c97
Ken Lee <cb3lee@uwaterloo.ca>
2017-10-27 11:24:33 +0000

    #itsworkingbutshitty

--- server.py
+++ server.py
@@ -4,14 +4,31 @@ from flask import Flask
 app = Flask(__name__)
 c = LightController()
 
+@app.route('/')
+def index():
+  return '''
+    <style>a { font-size: 300%; } div {display: block; margin: 20px;}</style>
+    <div><a href="/on">on</a></div>
+    <div><a href="/off">off</a></div>
+  '''
+
 @app.route('/on')
 def on():
   c.on()
-  return 'light is ON'
+  return '''
+    <style>a { font-size: 300%; } div {display: block; margin: 20px;}</style>
+    <div><a href="/on">on</a></div>
+    <div><a href="/off">off</a></div>
+  '''
 
 @app.route('/off')
 def off():
   c.off()
-  return 'light is OFF'
+  return '''
+    <style>a { font-size: 300%; } div {display: block; margin: 20px;}</style>
+    <div><a href="/on">on</a></div>
+    <div><a href="/off">off</a></div>
+  '''
 
 app.run(host='0.0.0.0', port=4000)
+
