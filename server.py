41eec497690bc4d08dcb888eea8e88ccf68235f1
Ken Lee <cb3lee@uwaterloo.ca>
2017-10-27 02:44:34 +0000

    first version

--- server.py
+++ server.py
@@ -7,8 +7,11 @@ c = LightController()
 @app.route('/on')
 def on():
   c.on()
+  return 'light is ON'
 
 @app.route('/off')
 def off():
   c.off()
+  return 'light is OFF'
 
+app.run(host='0.0.0.0', port=4000)
